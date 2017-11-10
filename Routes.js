7dd4325df31c9100e4f8507d22e3c4c056cbcfd8
Dom <1034971@ucn.dk>
2017-11-10 17:03:57 +0100

    fixed routes

--- Routes.js
+++ Routes.js
@@ -1,15 +1,15 @@
 (function () {
     "use-strict";
     var Promise = require('promise'),
-        userManagement = require('./UserManagement.js'),
-        serverManagement = require('./ServerManagement.js');
+        userManagement = require('./UserManagement.js');
+        //serverManagement = require('./ServerManagement.js');
 
     module.exports = function (app) {
         app.get('./getUsers', userManagement.getUsers);
-        app.get('/getUserById/:id', userManagement.getUserById);
-        app.get('/getUserByName/:name', userManagement.getUserByName);
-        app.get('/getLatestScoreFromUserId/:id', userManagement.getLatestScoreFromUserId);
-        app.get('/getUserTopScoresFromUserId/:id', userManagement.getUserTopScoresFromUserId);
+        app.post('/getUserById/:id', userManagement.getUserById);
+        app.post('/getUserByName/:name', userManagement.getUserByName);
+        app.post('/getLatestScoreFromUserId/:id', userManagement.getLatestScoreFromUserId);
+        app.post('/getUserTopScoresFromUserId/:id', userManagement.getUserTopScoresFromUserId);
         //app.get('/getServers', serverManagement.getServers);
         //app.post('/deleteUserById/:id', serverManagement.deleteUserById);
         app.post('/deleteUserByName/:name', userManagement.deleteUserByName);
