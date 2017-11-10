5f26be740b7de1873d425482b273ab209e61b416
Dom <1034971@ucn.dk>
2017-11-10 09:18:00 +0100

    Initial commit

---
+++ Main.js
@@ -0,0 +1,7 @@
+(function () {
+    "use-strict";
+    var express = require('express'),
+        Promise = require('promise'),
+        app = express();
+
+}());
---
+++ Routes.js
@@ -0,0 +1,16 @@
+(function () {
+    "use-strict";
+    var Promise = require('promise'),
+        userManagement = require('./UserManagement.js'),
+        serverManagement = require('./ServerManagement.js');
+
+    module.exports = function (app) {
+        app.get('/getUserById/:id', userManagement.getUserById);
+        app.get('/getUserByName/:name', userManagement.getUserByName);
+        app.get('./getLatestScoreFromUserId/:id', userManagement.getLatestScoreFromUserId);
+        app.get('./getUserTopScoresFromUserId/:id', userManagement.getUserTopScoresFromUserId);
+        app.get('./getServers', serverManagement.getServers);
+        app.post('./deleteUserById/:id', serverManagement.deleteUserById);
+        app.post('./deleteUserByName/:name', userManagement.deleteUserByName);
+    };
+}());
