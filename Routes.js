9e5b63d754aff5f7e23b7f6c0401330a3294c728
Dom <1034971@ucn.dk>
2017-11-10 16:55:15 +0100

    serverManagement Temporary removal

--- Routes.js
+++ Routes.js
@@ -10,8 +10,8 @@
         app.get('/getUserByName/:name', userManagement.getUserByName);
         app.get('/getLatestScoreFromUserId/:id', userManagement.getLatestScoreFromUserId);
         app.get('/getUserTopScoresFromUserId/:id', userManagement.getUserTopScoresFromUserId);
-        app.get('/getServers', serverManagement.getServers);
-        app.post('/deleteUserById/:id', serverManagement.deleteUserById);
+        //app.get('/getServers', serverManagement.getServers);
+        //app.post('/deleteUserById/:id', serverManagement.deleteUserById);
         app.post('/deleteUserByName/:name', userManagement.deleteUserByName);
     };
 }());
